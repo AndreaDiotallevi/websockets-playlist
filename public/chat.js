0d415d2d54e40f8dbaf21c9c8b1a779f24c85a30
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-14 17:24:31 +0100

    Add broadcasting

--- public/chat.js
+++ public/chat.js
@@ -2,10 +2,11 @@
 const socket = io.connect("http://localhost:4000");
 
 // Query DOM
-const message = document.getElementById("message");
-const handle = document.getElementById("handle");
-const btn = document.getElementById("send");
-const output = document.getElementById("output");
+const message = document.getElementById("message"),
+  handle = document.getElementById("handle"),
+  btn = document.getElementById("send"),
+  output = document.getElementById("output"),
+  feedback = document.getElementById("feedback");
 
 // Emit events
 btn.addEventListener("click", () => {
@@ -15,8 +16,17 @@ btn.addEventListener("click", () => {
   });
 });
 
+message.addEventListener("keypress", () => {
+  socket.emit("typing", handle.value);
+});
+
 // Listen for events
 socket.on("chat", (data) => {
+  feedback.innerHTML = "";
   output.innerHTML +=
     "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+});
+
+socket.on("typing", (data) => {
+  feedback.innerHTML = "<p><em>" + data + " is typing a message...</em</p>";
 });
